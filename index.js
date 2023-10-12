417ef2592618b1de11398751945edf17cf16048b
CkillDy_Artistic <95588502+CkillDy@users.noreply.github.com>
2023-10-12 16:24:50 -0300

    Primeira versão do cronômetro

---
+++ index.js
@@ -0,0 +1,70 @@
+const timeElement = document.getElementById("time")
+let initTime = 0
+let isRunning = false
+let startTIme = null
+
+const formatTime = (milliseconds) => {
+  const data = new Date(milliseconds * 1000)
+
+  const hours = data.getUTCHours().toString().padStart(2, "0")
+  const minutes = data.getUTCMinutes().toString().padStart(2, "0")
+  const seconds = data.getUTCSeconds().toString().padStart(2, "0")
+  return `${hours}:${minutes}:${seconds}`
+
+}
+
+const start = () => {
+  if (isRunning === false) {
+    console.log("O time foi iniciado com sucesso!")
+    startTIme = setInterval(() => {
+      initTime += 1
+      const time = formatTime(initTime)
+      timeElement.textContent = time
+    }, 1000);
+    isRunning = true
+  }
+}
+
+const pause = () => {
+  if (isRunning === true) {
+    console.log("O time foi pausado com sucesso!")
+    clearInterval(startTIme)
+    isRunning = false
+  }
+}
+
+const reset = () => {
+  if (initTime > 0) {
+    console.log("O time foi resetado com sucesso!")
+    timeElement.innerText = formatTime(0)
+    clearInterval(startTIme)
+    isRunning = false
+    initTime = 0
+    startTIme = null
+  }
+}
+
+async function startCronometro() {
+  const containerButtons = document.getElementById("buttons")
+  containerButtons.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const bottonclidado = e.target
+    switch (bottonclidado.id) {
+      case "start":
+        start()
+        break
+      case 'pause':
+        pause()
+        break
+      case "reset":
+        reset()
+        break
+      default:
+        return
+    }
+
+  })
+}
+
+startCronometro()
